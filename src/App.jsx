4cf765b957b18913fd8e8e7e3dbc2324e990bfc5
Samay Jain <samay08jain7@gmail.com>
2024-12-09 17:14:50 +0530

    built version 1.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,45 @@
-
 import { useState, useEffect } from "react";
 
-function Card({ name, image }) {
+function Card({ name, image, status, setScore, setBestScore, setPokemons, score }) {
+  
+  function handleClick() {
+    function updatePokemons(array) {
+      let i = array.findIndex(element => element.name === name);
+      let a = [...array]
+      a[i] = {name, image, chosen:true}
+      return a;
+    }
+    if(status == false){
+      setScore(s => s + 1);
+      setPokemons(poke => shuffleArray(updatePokemons(poke)));
+    }
+    else {
+      setBestScore(bs => Math.max(score, bs));
+      setScore(0);
+      setPokemons(poke => shuffleArray(poke.map(p => ({name:p.name,image:p.image,chosen : false}))));
+    }
+  }
+
   return (
-    <div className="p-4 text-center border border-white">
+    <div className="p-4 text-center border border-white card" onClick={handleClick}>
       <img src={image} alt={name} className="h-32 mx-auto" />
       <p> {name} </p>
     </div>
   );
+}
+
+function shuffleArray(arr) {
+  let array = [...arr];
+  let currentIndex= array.length;
+
+  while(currentIndex != 0){
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+
+  }
+  return array;
 }
 
 export default function App() {
@@ -28,15 +60,20 @@ export default function App() {
         const fetchedPokemons = responses.map((json) => ({
           name: json.name,
           image: json.sprites.front_default,
+          chosen: false,
         }));
-        setPokemons(fetchedPokemons); 
+        setPokemons(shuffleArray(fetchedPokemons)); 
       } catch (err) {
         console.log(err);
       }
     };
 
     fetchPokemons();
+    
   }, []);
+
+  
+
 
   return (
     <div className="bg-zinc-900 text-zinc-300 min-h-screen p-8">
@@ -52,7 +89,7 @@ export default function App() {
 
       <div className="grid grid-cols-4 gap-4">
         {pokemons.map((pokemon) => (
-          <Card key={pokemon.name} name={pokemon.name} image={pokemon.image} />
+          <Card key={pokemon.name} name={pokemon.name} image={pokemon.image} status={pokemon.chosen} setScore={setScore} setBestScore={setBestScore} setPokemons={setPokemons} score={score}/>
         ))}
       </div>
     </div>
